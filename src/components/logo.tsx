3517a3fe07cf0bcbc4261812264d8579f2b76128
KingTingTheGreat <jting@bu.edu>
2023-10-31 17:04:47 -0400

    created text logo

---
+++ src/components/logo.tsx
@@ -0,0 +1,7 @@
+export function Logo() {
+    return (
+        <h1 className="text-logo-red text-4xl font-bold leading-[28px]">
+            <span className="text-logo-green">NerdNest</span>3D
+        </h1>
+    )
+}
