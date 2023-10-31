f2b5e7550ed31f14154287d3a85f8832b6d7ba1a
KingTingTheGreat <jting@bu.edu>
2023-10-31 19:20:14 -0400

    updated main

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./tailwind.css";
 import "./reset.css";
 import App from "./App";
-import { RUBBER_DUCKY, SCROOGE } from "./data";
+import { products, users } from "./data";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App product={RUBBER_DUCKY} user={SCROOGE} />
+    <App product={products()} user={users()} />
   </React.StrictMode>
 );
