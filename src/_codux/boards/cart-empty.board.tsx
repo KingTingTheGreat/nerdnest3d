82ae71e44e0837f32e1e1403861abbf3e3b1b361
KingTingTheGreat <jting@bu.edu>
2023-10-31 16:32:33 -0400

    Initial commit

---
+++ src/_codux/boards/cart-empty.board.tsx
@@ -0,0 +1,13 @@
+import { createBoard } from '@wixc3/react-board';
+import { CartIcon } from '../../components/icons';
+import { RUBBER_DUCKY } from '../../data';
+
+export default createBoard({
+    name: 'Cart (empty)',
+    Board: () => (
+        <div>
+            <CartIcon cart={[]} />
+        </div>
+    ),
+    isSnippet: true,
+});
