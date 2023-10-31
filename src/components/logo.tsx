867f745ad3b6e95b8c0ee9182d05181c160de156
KingTingTheGreat <jting@bu.edu>
2023-10-31 17:30:24 -0400

    logo component

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,6 +1,6 @@
 export function Logo() {
     return (
-        <h1 className="text-logo-red text-4xl font-bold leading-[28px]">
+        <h1 className="text-logo-purple text-4xl font-bold leading-[28px]">
             <span className="text-logo-green">NerdNest</span>3D
         </h1>
     )
