167068a72cd23b3a956cb29ce85130fbb8dafb00
KingTingTheGreat <jting@bu.edu>
2023-10-31 19:01:51 -0400

    moved product and user data to their own directories

--- src/data.ts
+++ src/data.ts
@@ -1,68 +1,14 @@
-import imgUrl1 from "./assets/images/product-images/rubber-ducky/duckies_1.png";
-import imgUrl2 from "./assets/images/product-images/rubber-ducky/duckies_2.jpg";
-import imgUrl3 from "./assets/images/product-images/rubber-ducky/duckies_3.jpg";
-import imgUrl4 from "./assets/images/product-images/rubber-ducky/duckies_4.jpg";
-import imgUrl5 from "./assets/images/product-images/rubber-ducky/duckies_5.jpg";
+import { RUBBER_DUCKY } from "./products/rubber-ducky";
+import { SCROOGE } from "./users/scrooge";
 
-export const RUBBER_DUCKY = {
-  id: "1234",
-  name: "Rubber Ducky Debugging Buddy",
-  description:
-    "Never code alone when you've got this rubber ducky on your desk!",
-  features: [
-    "Always listens to you",
-    "Never judges you",
-    "Helps you become the best coder you can be",
-    "Floats",
-  ],
-  images: [
-    {
-      url: imgUrl1,
-      title: "The most loyal & helpful coworker you will ever have",
-      alt: "sitting on a laptop keyboard",
-    },
-    {
-      url: imgUrl2,
-      title: "No matter how big the bug, ducky is there for you",
-      alt: "sitting next to another ducky, both facing the camera",
-    },
-    {
-      url: imgUrl3,
-      alt: "sitting next to another ducky, both in profile facing left",
-    },
-    {
-      url: imgUrl4,
-      alt: "swimming in different directions on a blue surface",
-    },
-    {
-      url: imgUrl5,
-      alt: "sitting in profile facing another ducky sitting facing the camera",
-    },
-  ],
-  reviews: [
-    { rating: 5, text: "So impressed!", date: new Date(2023, 6, 13).valueOf() },
-    { rating: 5, date: new Date(2023, 4, 10).valueOf() },
-    {
-      rating: 4,
-      text: "Pretty good duck. I just wish it came with sunglasses.",
-      date: new Date(2023, 1, 1).valueOf(),
-    },
-    {
-      rating: 1,
-      text: "Useless. Did not fix my bug :(",
-      date: new Date(2022, 12, 15).valueOf(),
-    },
-    { rating: 4, date: new Date(2022, 11, 11).valueOf() },
-    {
-      rating: 3,
-      text: "Meh, ok I guess",
-      date: new Date(2022, 10, 30).valueOf(),
-    },
-  ],
-  price: 29.95,
-  sizes: ["S", "M", "L", "XL"],
+export function products() {
+  return (
+    RUBBER_DUCKY
+  )
 };
 
-export const SCROOGE = {
-  name: "Scrooge McDuck",
+export function users() {
+  return (
+    SCROOGE
+  )
 };
---
+++ src/users/scrooge.ts
@@ -0,0 +1,3 @@
+export const SCROOGE = {
+    name: "Scrooge McDuck",
+};
