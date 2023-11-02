5011b0a4c5fa4e44a81dd1f08525bfd4eeae00d7
KingTingTheGreat <jting@bu.edu>
2023-11-02 14:36:05 -0400

    standardize aspect ratio of preview images

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,16 +1,16 @@
-// import * as AspectRatio from '@radix-ui/react-aspect-ratio';
+import * as AspectRatio from '@radix-ui/react-aspect-ratio';
 import type { Image } from '../types';
 
 export const Preview = ({ image }: { image: Image }) => {
     return (
         <div className="relative w-full">
-            {/* <AspectRatio.Root className="relative" key="active" ratio={1 / 1}> */}
-            <img
-                className="h-full bg-gray-med object-cover max-width-xs"
-                src={image.url}
-                alt={image.alt || image.title}
-            />
-            {/* </AspectRatio.Root> */}
+            <AspectRatio.Root className="relative" key="active" ratio={1 / 1}>
+                <img
+                    className="h-full bg-gray-med object-cover max-width-xs"
+                    src={image.url}
+                    alt={image.alt || image.title}
+                />
+            </AspectRatio.Root>
         </div>
     );
 };
