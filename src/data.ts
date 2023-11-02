82f45ba5302d06db42a14958d1cf226492fabb84
KingTingTheGreat <jting@bu.edu>
2023-11-02 11:43:49 -0400

    exports list of products

--- src/data.ts
+++ src/data.ts
@@ -1,13 +1,14 @@
 import { RUBBER_DUCKY } from "./products/rubber-ducky";
+import { SABER_STAND } from "./products/saber-stand";
 import { SCROOGE } from "./users/scrooge";
 
 export function products() {
   return (
-    RUBBER_DUCKY
+    [RUBBER_DUCKY, SABER_STAND]
   )
 };
 
-export function users() {
+export function user() {
   return (
     SCROOGE
   )
