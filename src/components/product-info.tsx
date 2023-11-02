3ff58329729786c9ee57b3dc4190e7cc5d651c73
KingTingTheGreat <jting@bu.edu>
2023-11-02 11:49:18 -0400

    formatting

--- src/components/product-info.tsx
+++ src/components/product-info.tsx
@@ -68,3 +68,35 @@ export const SizeToggle = ({
     </div>
   );
 };
+
+export const ColorToggle = ({
+  sizes,
+  onChange,
+}: {
+  sizes: Product["colors"];
+  onChange: (s: string) => void;
+}) => {
+  return (
+    <div className="flex flex-col items-start gap-1 w-40  font-medium">
+      <label className="leading-tight">Color</label>
+      <ToggleGroup.Root
+        type="single"
+        aria-label="Product size"
+        onValueChange={onChange}
+        className="w-full grid grid-flow-col bg-gray-light rounded"
+      >
+        {sizes.map((size, i) => (
+          <ToggleGroup.Item
+            className="bg-gray-light data-[state=on]:bg-yellow px-5 py-2 rounded cursor-pointer"
+            key={i}
+            value={size}
+            title={`Color ${size}`}
+            aria-label={`Color ${size}`}
+          >
+            {size}
+          </ToggleGroup.Item>
+        ))}
+      </ToggleGroup.Root>
+    </div>
+  );
+};
