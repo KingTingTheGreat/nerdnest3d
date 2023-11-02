3a14845c27ef4b1ca34f3ea53af8eee65c765279
KingTingTheGreat <jting@bu.edu>
2023-11-02 13:39:35 -0400

    formatting

--- src/components/product-preview.tsx
+++ src/components/product-preview.tsx
@@ -4,17 +4,15 @@ import { Preview } from './preview';
 
 export const ProductPreview = ({ products }: { products: Product[] }) => {
     return (
-        <div className="display-grid">
+        <div className="grid grid-cols-6">
             {products.map(function (product: Product) {
                 const { id, preview, name, price, description, reviews } =
                     product;
                 return (
-                    <div className="flex flex-col gap-5 p-5 grid-item" key={id}>
+                    <div className="grid-item p-5" key={id}>
                         <Preview image={preview} />
-                        <h2 className="flex justify-between text-3xl font-normal">
-                            <span className="w-2/3">{name}</span>
-                            <span>{price}</span>
-                        </h2>
+                        <h2>{name}</h2>
+                        <h2>{price}</h2>
                         <p>{description}</p>
                         <RatingAverage reviews={reviews} />
                     </div>
