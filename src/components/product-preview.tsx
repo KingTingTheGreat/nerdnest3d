e6d76390248a0866e6d34d2792a4caac3a996c9d
KingTingTheGreat <jting@bu.edu>
2023-11-02 15:58:09 -0400

    center content not in main

--- src/components/product-preview.tsx
+++ src/components/product-preview.tsx
@@ -4,12 +4,13 @@ import { Preview } from './preview';
 
 export const ProductPreview = ({ products }: { products: Product[] }) => {
     return (
-        <div className="grid grid-cols-6">
+        // <div className="grid grid-cols-6">
+        <div className="flex flex-wrap width-4/5 justify-center">
             {products.map(function (product: Product) {
                 const { id, preview, name, price, description, reviews } =
                     product;
                 return (
-                    <div className="grid-item p-5" key={id}>
+                    <div className="flex flex-col p-5 m-2 width-full" key={id}>
                         <Preview image={preview} />
                         <h2>{name}</h2>
                         <h2>{price}</h2>
