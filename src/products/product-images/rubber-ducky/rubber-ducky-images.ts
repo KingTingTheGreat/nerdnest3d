072d16b2453656c7967faed9ff6cc54478117c22
KingTingTheGreat <jting@bu.edu>
2023-11-02 12:03:58 -0400

    import url rather than image... i think

--- src/products/product-images/rubber-ducky/rubber-ducky-images.ts
+++ src/products/product-images/rubber-ducky/rubber-ducky-images.ts
@@ -1,30 +1,30 @@
-import * as img1 from "./duckies_1.png";
-import * as img2 from "./duckies_2.jpg";
-import * as img3 from "./duckies_3.jpg";
-import * as img4 from "./duckies_4.jpg";
-import * as img5 from "./duckies_5.jpg";
+import img1 from './duckies_1.png';
+import img2 from './duckies_2.jpg';
+import img3 from './duckies_3.jpg';
+import img4 from './duckies_4.jpg';
+import img5 from './duckies_5.jpg';
 
 export const images = [
     {
         url: img1,
-        title: "The most loyal & helpful coworker you will ever have",
-        alt: "sitting on a laptop keyboard",
+        title: 'The most loyal & helpful coworker you will ever have',
+        alt: 'sitting on a laptop keyboard',
     },
     {
         url: img2,
-        title: "No matter how big the bug, ducky is there for you",
-        alt: "sitting next to another ducky, both facing the camera",
+        title: 'No matter how big the bug, ducky is there for you',
+        alt: 'sitting next to another ducky, both facing the camera',
     },
     {
         url: img3,
-        alt: "sitting next to another ducky, both in profile facing left",
+        alt: 'sitting next to another ducky, both in profile facing left',
     },
     {
         url: img4,
-        alt: "swimming in different directions on a blue surface",
+        alt: 'swimming in different directions on a blue surface',
     },
     {
         url: img5,
-        alt: "sitting in profile facing another ducky sitting facing the camera",
+        alt: 'sitting in profile facing another ducky sitting facing the camera',
     },
-]
+];
--- src/products/product-images/saber-stand/saber-stand-images.ts
+++ src/products/product-images/saber-stand/saber-stand-images.ts
@@ -1,20 +1,20 @@
-import * as img1 from "./saber-stand-1.png";
-import * as img2 from "./saber-stand-2.jpeg";
-import * as img3 from "./saber-stand-3.jpg";
+import img1 from './saber-stand-1.png';
+import img2 from './saber-stand-2.jpeg';
+import img3 from './saber-stand-3.jpg';
 
 export const images = [
     {
         url: img1,
-        title: "The most loyal & helpful coworker you will ever have",
-        alt: "mounted on a wall",
+        title: 'The most loyal & helpful coworker you will ever have',
+        alt: 'mounted on a wall',
     },
     {
         url: img2,
-        title: "No matter how big the bug, ducky is there for you",
-        alt: "showing how the light sword is mounted",
+        title: 'No matter how big the bug, ducky is there for you',
+        alt: 'showing how the light sword is mounted',
     },
     {
         url: img3,
-        alt: "shows how the product is mounted to the wall",
+        alt: 'shows how the product is mounted to the wall',
     },
-]
+];
