42f6d253f670345b6809385adbda0bbd4844c5ce
KingTingTheGreat <jting@bu.edu>
2023-11-02 11:11:33 -0400

    fixed path to images

--- src/products/product-images/rubber-ducky/rubber-ducky-images.ts
+++ src/products/product-images/rubber-ducky/rubber-ducky-images.ts
@@ -1,5 +1,5 @@
-export * as img1 from "./product-images/rubber-ducky/duckies_1.png";
-export * as img2 from "./product-images/rubber-ducky/duckies_2.jpg";
-export * as img3 from "./product-images/rubber-ducky/duckies_3.jpg";
-export * as img4 from "./product-images/rubber-ducky/duckies_4.jpg";
-export * as img5 from "./product-images/rubber-ducky/duckies_5.jpg";
+export * as img1 from "./duckies_1.png";
+export * as img2 from "./duckies_2.jpg";
+export * as img3 from "./duckies_3.jpg";
+export * as img4 from "./duckies_4.jpg";
+export * as img5 from "./duckies_5.jpg";
--- src/products/product-images/saber-stand/saber-stand-images.ts
+++ src/products/product-images/saber-stand/saber-stand-images.ts
@@ -1,3 +1,3 @@
-export * as img1 from "./product-images/saber-stand/saber-stand-1.png";
-export * as img2 from "./product-images/saber-stand/saber-stand-2.jfif";
-export * as img3 from "./product-images/saber-stand/saber-stand-3.jpg";
+export * as img1 from "./saber-stand-1.png";
+export * as img2 from "./saber-stand-2.jfif";
+export * as img3 from "./saber-stand-3.jpg";
