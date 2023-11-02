da2e93911a08806d3e632667be5d4c3daf70e255
KingTingTheGreat <jting@bu.edu>
2023-11-02 11:59:54 -0400

    correct preview_image to preview

--- src/products/rubber-ducky.ts
+++ src/products/rubber-ducky.ts
@@ -1,39 +1,43 @@
-import { images } from "./product-images/rubber-ducky/rubber-ducky-images";
+import { images } from './product-images/rubber-ducky/rubber-ducky-images';
 
 export const RUBBER_DUCKY = {
-    id: "1234",
-    name: "Rubber Ducky: Debugging Buddy",
+    id: '1234',
+    name: 'Rubber Ducky: Debugging Buddy',
     description:
         "Never code alone when you've got this rubber ducky on your desk!",
     features: [
-        "Always listens to you",
-        "Never judges you",
-        "Helps you become the best coder you can be",
-        "Floats",
+        'Always listens to you',
+        'Never judges you',
+        'Helps you become the best coder you can be',
+        'Floats',
     ],
-    preview_image: images[0],
+    preview: images[0],
     images: images,
     reviews: [
-        { rating: 5, text: "So impressed!", date: new Date(2023, 6, 13).valueOf() },
+        {
+            rating: 5,
+            text: 'So impressed!',
+            date: new Date(2023, 6, 13).valueOf(),
+        },
         { rating: 5, date: new Date(2023, 4, 10).valueOf() },
         {
             rating: 4,
-            text: "Pretty good duck. I just wish it came with sunglasses.",
+            text: 'Pretty good duck. I just wish it came with sunglasses.',
             date: new Date(2023, 1, 1).valueOf(),
         },
         {
             rating: 1,
-            text: "Useless. Did not fix my bug :(",
+            text: 'Useless. Did not fix my bug :(',
             date: new Date(2022, 12, 15).valueOf(),
         },
         { rating: 4, date: new Date(2022, 11, 11).valueOf() },
         {
             rating: 3,
-            text: "Meh, ok I guess",
+            text: 'Meh, ok I guess',
             date: new Date(2022, 10, 30).valueOf(),
         },
     ],
     price: 9.95,
-    sizes: ["S", "M", "L", "XL"],
-    colors: ["yellow"],
+    sizes: ['S', 'M', 'L', 'XL'],
+    colors: ['yellow'],
 };
--- src/products/saber-stand.ts
+++ src/products/saber-stand.ts
@@ -1,38 +1,41 @@
-import { images } from "./product-images/saber-stand/saber-stand-images";
+import { images } from './product-images/saber-stand/saber-stand-images';
 
 export const SABER_STAND = {
-    id: "0104",
-    name: "Saber Stand",
-    description:
-        "A novel way to display your most valued light swords!",
+    id: '0104',
+    name: 'Saber Stand',
+    description: 'A novel way to display your most valued light swords!',
     features: [
-        "Displays your light swords",
-        "Easily attaches and detaches from the wall",
-        "Light swords can effortlessly be hung up and brought down",
+        'Displays your light swords',
+        'Easily attaches and detaches from the wall',
+        'Light swords can effortlessly be hung up and brought down',
     ],
-    preview_image: images[0],
+    preview: images[0],
     images: images,
     reviews: [
-        { rating: 5, text: "So impressed!", date: new Date(2023, 6, 13).valueOf() },
+        {
+            rating: 5,
+            text: 'So impressed!',
+            date: new Date(2023, 6, 13).valueOf(),
+        },
         { rating: 5, date: new Date(2023, 4, 10).valueOf() },
         {
             rating: 4,
-            text: "Pretty good duck. I just wish it came with sunglasses.",
+            text: 'Pretty good duck. I just wish it came with sunglasses.',
             date: new Date(2023, 1, 1).valueOf(),
         },
         {
             rating: 1,
-            text: "Useless. Did not fix my bug :(",
+            text: 'Useless. Did not fix my bug :(',
             date: new Date(2022, 12, 15).valueOf(),
         },
         { rating: 4, date: new Date(2022, 11, 11).valueOf() },
         {
             rating: 3,
-            text: "Meh, ok I guess",
+            text: 'Meh, ok I guess',
             date: new Date(2022, 10, 30).valueOf(),
         },
     ],
     price: 29.95,
-    sizes: ["S", "M", "L", "XL"],
-    colors: ["black", "white", "orange", "pink"],
+    sizes: ['S', 'M', 'L', 'XL'],
+    colors: ['black', 'white', 'orange', 'pink'],
 };
