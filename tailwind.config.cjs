675664260a08a4539d95562b00da95d9517188c2
KingTingTheGreat <jting@bu.edu>
2023-11-03 19:04:26 -0400

    easier usage of typical css options

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 
-// defaults values for height and width to percent
-const percentConfig = {};
+// default values for height and width are percetage
+// also allows for vw and vh usages
+const vwConfig = {};
+const vhConfig = {};
 for (let i = 1; i <= 100; i++) {
-  percentConfig[i] = `${i}%`;
-};
+  // adding percentage values
+  vwConfig[`${i}%`] = `${i}%`;
+  vhConfig[`${i}%`] = `${i}%`;
+
+  // adding vw and vh values
+  vwConfig[`${i}vw`] = `${i}vw`;
+  vhConfig[`${i}vh`] = `${i}vh`;
+}
 
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -21,8 +29,10 @@ module.exports = {
         'logo-purple': "#9B59B6",
         'logo-green': "#26C281",
       },
-      width: percentConfig,
-      height: percentConfig,
+      width: vwConfig,
+      height: vhConfig,
+      minWidth: vwConfig, 
+      minHeight: vhConfig,
     },
   },
   plugins: [],
