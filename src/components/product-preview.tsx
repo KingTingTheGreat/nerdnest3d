714d6681fa41ff3a4ffd565044209c96d0776189
KingTingTheGreat <jting@bu.edu>
2023-11-03 19:05:42 -0400

    styling

--- src/components/product-preview.tsx
+++ src/components/product-preview.tsx
@@ -4,21 +4,25 @@ import { Preview } from './preview';
 
 export const ProductPreview = ({ products }: { products: Product[] }) => {
     return (
-        // <div className="grid grid-cols-6">
-        <div className="flex flex-wrap w-9/10 justify-center">
+        // <div className="flex justify-center items-center w-full">
+        <div className="flex flex-wrap w-90%">
             {products.map(function (product: Product) {
                 const { id, preview, name, price, description, reviews } =
                     product;
                 return (
-                    <div className="flex flex-col p-5 m-2 w-1/5" key={id}>
+                    <div
+                        className="flex flex-col p-4 m-2 w-15% min-w-10vw"
+                        key={id}
+                    >
                         <Preview image={preview} />
                         <h2>{name}</h2>
+                        <RatingAverage reviews={reviews} />
                         <h2>{price}</h2>
                         <p>{description}</p>
-                        <RatingAverage reviews={reviews} />
                     </div>
                 );
             })}
         </div>
+        // </div>
     );
 };
