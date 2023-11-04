fd8979e9bfc33acf0cc2e349e180d101b39492bc
KingTingTheGreat <jting@bu.edu>
2023-11-04 07:24:49 -0400

    logo links to home

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom';
+
 export function Logo() {
     return (
-        <h1 className="text-logo-purple text-4xl font-bold leading-[28px]">
-            <span className="text-logo-green">NerdNest</span>3D
-        </h1>
+        <Link to="/">
+            <h1 className="text-logo-purple text-4xl font-bold leading-[28px]">
+                <span className="text-logo-green">NerdNest</span>3D
+            </h1>
+        </Link>
     );
 }
