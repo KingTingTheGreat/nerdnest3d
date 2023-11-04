a6656dcd71a6608dbf5430bb3b22db4b4be925e3
KingTingTheGreat <jting@bu.edu>
2023-11-04 07:30:45 -0400

    individual product page. added color selector

--- src/components/product-preview.tsx
+++ src/components/product-preview.tsx
@@ -6,23 +6,24 @@ import { Link } from 'react-router-dom';
 export const ProductPreview = ({ products }: { products: Product[] }) => {
     return (
         // <div className="flex justify-center items-center w-full">
-        <div className="flex flex-wrap w-90%">
+        <div className="flex flex-wrap w-90% bg-white">
             {products.map(function (product: Product) {
                 const { id, preview, name, price, description, reviews } =
                     product;
                 return (
-                    <Link to={`/products/${product.name}`}>
-                        <div
-                            className="flex flex-col p-4 m-2 w-15% min-w-10vw"
-                            key={id}
-                        >
+                    <div
+                        className="flex flex-col p-4 m-2 w-15% min-w-10vw"
+                        key={id}
+                    >
+                        <Link to={`/products/${product.id}`}>
                             <Preview image={preview} />
                             <h2>{name}</h2>
                             <RatingAverage reviews={reviews} />
                             <h2>{price}</h2>
                             <p>{description}</p>
-                        </div>
-                    </Link>
+                        </Link>
+                    </div>
+                    // </Link>
                 );
             })}
         </div>
