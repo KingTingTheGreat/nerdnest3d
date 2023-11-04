f36ae0a05a8d9560e4592fcfab984deccb6a8109
KingTingTheGreat <jting@bu.edu>
2023-11-04 07:50:15 -0400

    paths to different pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,39 @@ import { ProductGrid } from './components/product-grid';
 import { useCart } from './hooks';
 import type { Product, User } from './types';
 import { ProductPreview } from './components/product-preview';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ProductPage } from './components/product-page';
 
 function App({ products, user }: { products: Product[]; user: User }) {
     const [cart, addToCart] = useCart();
 
     return (
         <div className="bg-gray-light">
-            <Header cart={cart} user={user} />
+            <BrowserRouter>
+                <Header cart={cart} user={user} />
 
-            <main className="content-center">
-                {/* <ProductGrid products={products} addToCart={addToCart} /> */}
-                <ProductPreview products={products} />
-            </main>
+                <main className="content-center">
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={<ProductPreview products={products} />}
+                        />
+                        <Route
+                            path="/products/:productId"
+                            element={<ProductPage addToCart={addToCart} />}
+                        />
+                    </Routes>
+                </main>
 
-            <footer className="text-center text-sm p-5">
-                Made with ♥ in&nbsp;
-                <a href="https://codux.com">Codux</a>
-                &nbsp;with&nbsp;
-                <a href="https://radix-ui.com">RadixUI</a>
-                &nbsp;and&nbsp;
-                <a href="https://tailwindcss.com">TailwindCSS</a>
-            </footer>
+                <footer className="text-center text-sm p-5">
+                    Made with ♥ in&nbsp;
+                    <a href="https://codux.com">Codux</a>
+                    &nbsp;with&nbsp;
+                    <a href="https://radix-ui.com">RadixUI</a>
+                    &nbsp;and&nbsp;
+                    <a href="https://tailwindcss.com">TailwindCSS</a>
+                </footer>
+            </BrowserRouter>
         </div>
     );
 }
