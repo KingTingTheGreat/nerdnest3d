844f47da82146cbbf48add6a09c5d1711adadd65
KingTingTheGreat <jting@bu.edu>
2023-11-04 07:02:36 -0400

    links to product page

--- src/components/product-preview.tsx
+++ src/components/product-preview.tsx
@@ -1,6 +1,7 @@
 import { RatingAverage } from './reviews';
 import type { Product } from '../types';
 import { Preview } from './preview';
+import { Link } from 'react-router-dom';
 
 export const ProductPreview = ({ products }: { products: Product[] }) => {
     return (
@@ -10,16 +11,18 @@ export const ProductPreview = ({ products }: { products: Product[] }) => {
                 const { id, preview, name, price, description, reviews } =
                     product;
                 return (
-                    <div
-                        className="flex flex-col p-4 m-2 w-15% min-w-10vw"
-                        key={id}
-                    >
-                        <Preview image={preview} />
-                        <h2>{name}</h2>
-                        <RatingAverage reviews={reviews} />
-                        <h2>{price}</h2>
-                        <p>{description}</p>
-                    </div>
+                    <Link to={`/products/${product.name}`}>
+                        <div
+                            className="flex flex-col p-4 m-2 w-15% min-w-10vw"
+                            key={id}
+                        >
+                            <Preview image={preview} />
+                            <h2>{name}</h2>
+                            <RatingAverage reviews={reviews} />
+                            <h2>{price}</h2>
+                            <p>{description}</p>
+                        </div>
+                    </Link>
                 );
             })}
         </div>
