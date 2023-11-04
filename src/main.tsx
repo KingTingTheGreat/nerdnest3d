0bf346cabea0d81b03065aa18178c48a66ecf8c6
KingTingTheGreat <jting@bu.edu>
2023-11-04 07:25:12 -0400

    adjusted to renamed variables

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './tailwind.css';
 import './reset.css';
 import App from './App';
-import { PRODUCTS, user } from './data';
+import { Products, user } from './data';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <App products={PRODUCTS} user={user()} />
+        <App products={Products} user={user()} />
     </React.StrictMode>
 );
