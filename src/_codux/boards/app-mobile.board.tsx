3d4fd2f6659d2520efde8be26c4791692f2ea6ce
KingTingTheGreat <jting@bu.edu>
2023-11-07 11:59:55 -0500

    fixed bug

--- src/_codux/boards/app-mobile.board.tsx
+++ src/_codux/boards/app-mobile.board.tsx
@@ -1,10 +1,9 @@
 import { createBoard } from '@wixc3/react-board';
 import App from '../../App';
-import { Products, user } from '../../data';
 
 export default createBoard({
     name: 'App (mobile)',
-    Board: () => <App products={Products} user={user()} />,
+    Board: () => <App />,
     environmentProps: {
         windowWidth: 400,
     },
--- src/_codux/boards/app.board.tsx
+++ src/_codux/boards/app.board.tsx
@@ -1,14 +1,9 @@
 import { createBoard } from '@wixc3/react-board';
 import App from '../../App';
-import { Products, user } from '../../data';
-
-// temporary stuffs.
-const RUBBER_DUCKY = Products[0];
-const SCROOGE = user();
 
 export default createBoard({
     name: 'App',
-    Board: () => <App products={Products} user={SCROOGE} />,
+    Board: () => <App />,
     isSnippet: true,
     environmentProps: {
         canvasWidth: 885,
