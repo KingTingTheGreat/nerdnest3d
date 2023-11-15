2dbd56d1106740729f5d742851cedfc4d92030e1
KingTingTheGreat <jting@bu.edu>
2023-11-15 14:15:04 -0500

    links to shop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,9 +17,9 @@ export default function Home() {
                 <div className="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
                     <div className="sm:py-15 mx-auto max-w-7xl py-16 px-4 sm:px-6 lg:px-8">
                         <div className="text-center">
-                            <p className="mt-1 text-4xl font-bold uppercase text-gray-900 sm:text-5xl sm:tracking-tight lg:text-5xl">
+                            <Link href="/shop" className="mt-1 text-4xl font-bold uppercase text-gray-900 sm:text-5xl sm:tracking-tight lg:text-5xl">
                                 Designed and printed by us, for you
-                            </p>
+                            </Link>
                         </div>
                     </div>
                     <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
